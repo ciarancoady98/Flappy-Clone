d11edb59689cca4b740de7e1abb11b8d8c605cc7
Ciaran Coady <46931188+ciarancoady98@users.noreply.github.com>
2019-05-26 20:16:23 +0100

    Random redraw position for crabs
    added random redraw position for crabs when the move off the screen.

--- Assets/Code/ObstacleMovement.cs
+++ Assets/Code/ObstacleMovement.cs
@@ -6,7 +6,10 @@ public class ObstacleMovement : MonoBehaviour
 {
     [SerializeField] private float _speed = (float)5;
     [SerializeField] private bool _randomizeHeight = true;
+    [SerializeField] private bool _randomRedrawPos = false;
     [SerializeField] private int _renderPlane = 0;
+
+    //we also want to implement random spacing of the crabs
 
     // Update is called once per frame
     void Update()
@@ -14,14 +17,25 @@ public class ObstacleMovement : MonoBehaviour
         transform.Translate(Vector3.left * Time.deltaTime * _speed);
         if(transform.position.x < -15)
         {
-            if (_randomizeHeight)
+            float xPos;
+            if (_randomRedrawPos)
             {
-                float yPos = UnityEngine.Random.Range(-3, 3);
-                transform.position = new Vector3(15, yPos, _renderPlane);
+                xPos = UnityEngine.Random.Range(15, 40);
             }
             else
             {
-                transform.position = new Vector3(15, transform.position.y, _renderPlane);
+                xPos = 15;
+            }
+
+
+            if (_randomizeHeight)
+            {
+                float yPos = UnityEngine.Random.Range(-3, 3);
+                transform.position = new Vector3(xPos, yPos, _renderPlane);
+            }
+            else
+            {
+                transform.position = new Vector3(xPos, transform.position.y, _renderPlane);
             }
         }
     }
