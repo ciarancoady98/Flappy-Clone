6d562e063951fcae58b8e3ff75dec263ce598812
Ciaran Coady <46931188+ciarancoady98@users.noreply.github.com>
2019-05-26 14:17:02 +0100

    Movement Script
    Added simple movement script to move the player up when a key is pressed

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -13,6 +13,12 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        bool pressedFireButton = Input.GetButton("Fire1");
+        if (pressedFireButton)
+        {
+            Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+            rigidbody.velocity = Vector3.zero;
+            rigidbody.AddForce(Vector3.up * 1000);
+        }
     }
 }
