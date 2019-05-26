59e175e5d9b9360069a65a995c05f8db4a77b941
Ciaran Coady <46931188+ciarancoady98@users.noreply.github.com>
2019-05-26 17:06:08 +0100

    Update to movement script
    added new options to the movement script to allow for various different movement behaviour

--- Assets/Code/MoveObstacleLeft.cs
+++ Assets/Code/MoveObstacleLeft.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class MoveObstacleLeft : MonoBehaviour
 {
     [SerializeField] private float _speed = (float)5;
+    [SerializeField] private bool _randomizeHeight = true;
+    [SerializeField] private bool _randomMovement = false;
 
     // Update is called once per frame
     void Update()
@@ -12,8 +14,20 @@ public class MoveObstacleLeft : MonoBehaviour
         transform.Translate(Vector3.left * Time.deltaTime * _speed);
         if(transform.position.x < -15)
         {
-            float yPos = UnityEngine.Random.Range(-3, 3);
-            transform.position = new Vector3(15, yPos, 0);
+            if (_randomizeHeight)
+            {
+                float yPos = UnityEngine.Random.Range(-3, 3);
+                transform.position = new Vector3(15, yPos, 0);
+            }
+            else if (_randomMovement)
+            {
+                //randomly change either the direction of velocity of the sprite
+
+            }
+            else
+            {
+                transform.position = new Vector3(15, transform.position.y, 0);
+            }
         }
     }
 }
