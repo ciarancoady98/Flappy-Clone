1877fbe79b7c9d989a20e7aa1313237fb50866d9
Ciaran Coady <46931188+ciarancoady98@users.noreply.github.com>
2019-05-26 19:55:54 +0100

    Script Changes
    Moved all crab movement functionality to its own script.
    
    finished crab movement pattern and code.
    
    made the crab collidable with the player

---
+++ Assets/Code/CrabRandomMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrabRandomMovement : MonoBehaviour
+{
+    [SerializeField] private bool _randomMovement = false;
+    [SerializeField] private float movementAmount = (float)0.1;
+    private int _direction = 1;
+
+    //we also want to implement random spacing of the crabs
+
+    //move the crab by the movement amount in the current direction
+    private void move()
+    {
+        float yPos = transform.position.y + (movementAmount * _direction);
+        transform.position = new Vector3(transform.position.x, yPos, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_randomMovement)
+        {
+            //randomly change either the direction of velocity of the sprite
+            if ((transform.position.y > -4.3 && _direction == -1) || (transform.position.y < -3.7 && _direction == 1))
+            {
+                //continue to move in direction
+                move();
+            }
+            else
+            {
+                //change direction
+                _direction = _direction * -1;
+                move();
+            }
+        }
+    }
+}
--- Assets/Code/ObstacleMovement.cs
+++ Assets/Code/ObstacleMovement.cs
@@ -6,11 +6,7 @@ public class ObstacleMovement : MonoBehaviour
 {
     [SerializeField] private float _speed = (float)5;
     [SerializeField] private bool _randomizeHeight = true;
-    [SerializeField] private bool _randomMovement = false;
     [SerializeField] private int _renderPlane = 0;
-    private int _direction = 1;
-
-    //we also want to implement random spacing of the crabs
 
     // Update is called once per frame
     void Update()
@@ -26,23 +22,6 @@ public class ObstacleMovement : MonoBehaviour
             else
             {
                 transform.position = new Vector3(15, transform.position.y, _renderPlane);
-            }
-        }
-        else if (_randomMovement)
-        {
-            //randomly change either the direction of velocity of the sprite
-            if (transform.position.y <= -3.7 && transform.position.y >= -4.3)
-            {
-                //continue to move in direction
-                float yPos = transform.position.y + (Time.deltaTime * _direction);
-                transform.position = new Vector3(transform.position.x, yPos, transform.position.z);
-            }
-            else
-            {
-                //change direction
-                _direction = _direction * -1;
-                float yPos = transform.position.y + (Time.deltaTime * _direction);
-                transform.position = new Vector3(transform.position.x, yPos, transform.position.z);
             }
         }
     }
