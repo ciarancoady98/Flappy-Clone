2b44d151b6aa5a7b96f2e91d87d47cb73ea5ce2e
Ciaran Coady <46931188+ciarancoady98@users.noreply.github.com>
2019-05-26 15:57:45 +0100

    Added Wrapping of obstacles
    Obstacles re-draw when they move off the screen

--- Assets/Code/MoveObstacleLeft.cs
+++ Assets/Code/MoveObstacleLeft.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class MoveObstacleLeft : MonoBehaviour
 {
+    [SerializeField] private float _speed = (float)5;
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime);
+        transform.Translate(Vector3.left * Time.deltaTime * _speed);
+        if(transform.position.x < -15)
+        {
+            transform.position = new Vector3(15, 0, 0);
+        }
     }
 }
