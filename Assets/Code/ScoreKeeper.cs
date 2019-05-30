913bc02cf4ff35afbde78c7526d09381d64de1f0
Ciaran Coady <46931188+ciarancoady98@users.noreply.github.com>
2019-05-30 23:54:57 +0100

    Score Updating
    Added a script that will increment the score displayed on screen when it is called

---
+++ Assets/Code/ScoreKeeper.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreKeeper : MonoBehaviour
+{
+    private int _currentScore = 0;
+
+    public void IncrementScore()
+    {
+        _currentScore++;
+        //Takes the score text and updates it to the new score value;
+        Text scoreText = GetComponent<Text>();
+        scoreText.text = "Score : " + _currentScore.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        IncrementScore();
+    }
+}
